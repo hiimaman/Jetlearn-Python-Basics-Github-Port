54e51a8cb3f8364ae8f38f2b42a272b69705abfe
hiimaman <116463770+hiimaman@users.noreply.github.com>
2024-08-11 20:22:31 +0100

    Create Datatype-Conversion(Slash)-Storing-Multiple-Variables.py

---
+++ Datatype-Conversion(Slash)-Storing-Multiple-Variables.py
@@ -0,0 +1,35 @@
+# Data type conversion and structures + is someone elegible to vote
+# a = float(input("Choose the first number\n"))
+# b = float(input("Choose the second number\n"))
+# print(a + b)
+# fN = input("What is your first name?\n")
+# sN = input("What is your sir name?\n")
+# print(fN + " " + sN)
+# x = 'Abdul'
+# y = 5
+# print(x + " has " + str(y) + " pets.")
+
+# List Example
+# d = [2, 3]
+# print(d)
+
+#Duple Example
+# i = (2, 8, 6, 10)
+# print(i)
+
+# Dictionary Example
+# q = {"name": "Abdul", "number": 5, "float": 3.141592, "hobby": "art"}
+# print(q['name'])
+# print(q.keys())
+# print(q.values())
+
+#Set Example
+# p = (1, 1)
+# print(p)
+
+# Check if someone is ellegible to vote
+age = int(input("What is your age?\n"))
+if age >= 18:
+  print("You are eligible")
+else:
+  print("You are not elegible")
