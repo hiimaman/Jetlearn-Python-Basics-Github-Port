f42aad4611012c1849992288d3661ec765488b06
hiimaman <116463770+hiimaman@users.noreply.github.com>
2024-08-11 20:14:15 +0100

    Create Screwing-Around-With-Operators.py
    I cant remember much

---
+++ Screwing-Around-With-Operators.py
@@ -0,0 +1,20 @@
+# activity 1
+a = int(input("Enter your first number "))
+b = int(input("Enter your second number "))
+print(a + b)
+print(a * b)
+print(a / b)
+print((a + b) / 2)
+print(a ** b)
+print(a // b)
+print(a % b)
+
+# activity 2
+z = int(input("Number f t-shirts purchased "))
+x = int(input("Number of trousers purchased "))
+tShirtPrice = z * 20
+trouserPrice = x * 50
+print("The t-shirt price is ", tShirtPrice)
+print("The trousers price is ", trouserPrice)
+totalAmount = trouserPrice + tShirtPrice
+print("Total amount payable is ", totalAmount)
