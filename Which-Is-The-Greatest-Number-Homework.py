ea8a52a244f3e2a6ee4e27352fda78dc368ab95f
hiimaman <116463770+hiimaman@users.noreply.github.com>
2024-08-13 15:50:01 +0100

    Create Which-Is-The-Greatest-Number-Homework.py
    I dont need chatGPT anymore!

---
+++ Which-Is-The-Greatest-Number-Homework.py
@@ -0,0 +1,21 @@
+# I did it myself, because I am cool now
+
+import random
+
+# Declares A, B and C
+a = random.randint(1, 100)
+b = random.randint(1, 100)
+c = random.randint(1, 100)
+
+# Printing the variables
+print(a, " ", b, " ", c, " ")
+
+# Comparing the numbers
+if a > b and a > c:
+  print(a, 'is the greatest')
+elif b > a and b > c:
+  print(b, 'is the greatest')
+elif c > a and c > b:
+  print(c, 'is the greatest')
+else:
+  print('BUG FOUND, PLEASE FIX')
