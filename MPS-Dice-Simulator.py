0a09efa8c4209b706236eab5404db87de02e4287
hiimaman <116463770+hiimaman@users.noreply.github.com>
2024-08-14 17:37:15 +0100

    Create MPS-Dice-Simulator.py

---
+++ MPS-Dice-Simulator.py
@@ -0,0 +1,13 @@
+# MPS Means My Personal Project
+# Who Needs A Dice When You Got A *DIGITAL* Dice?
+
+import random
+
+# Declare The List
+a = (1, 2, 3, 4, 5, 6)
+
+# Declare DR Variable (Dice Roll)
+dr = (random.sample(a, 1))
+
+# Print it
+print(dr)
