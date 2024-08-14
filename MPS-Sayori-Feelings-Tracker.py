10e018185d3e081dce20d28b760d8ef552ac2061
hiimaman <116463770+hiimaman@users.noreply.github.com>
2024-08-14 18:05:53 +0100

    Create MPS-Sayori-Feelings-Tracker.py
    DDLC Themed To Keep Me Interested

---
+++ MPS-Sayori-Feelings-Tracker.py
@@ -0,0 +1,12 @@
+# MPS Means My Personal Project
+# DDLC Themed Coding Project To See How Sayori Is Feeling (Hopefully Ok)
+import random
+
+# Options
+o = ('sad', 'depressed', 'suicidal', 'happy', 'amazing', 'great', 'angry', 'annoyed', 'anxious', 'miserable', 'dead', 'non-existent', 'deleted', 'powerful', 'the best', 'like an idiot', 'happy', 'content with the world')
+
+# Which Emotion She Is Feeling
+e = random.sample(o, 1)
+
+# Print
+print('Sayori is:', e)
